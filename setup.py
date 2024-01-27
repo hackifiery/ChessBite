8059d99f28821a33bb6e546dbe21d37cbac683dd
hackifiery <129475587+hackifiery@users.noreply.github.com>
2024-01-27 15:40:14 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='ChessBite',
+    version='0.1',
+    py_modules=['main', 'engine'],  # Add all your script/module names here
+    install_requires=['python-chess'],
+    entry_points={
+        'console_scripts': [
+            'chessbite=main:main',
+        ],
+    },
+    author='hackifiery',
+    url='https://github.com/hackifiery/ChessBite',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
